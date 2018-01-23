e9541cb093a23d4689a0f6484091b320073ed3de
James Eagle <jimeagle@gmail.com>
2018-01-23 00:08:18 +0000

    Start experience table

--- experience.go
+++ experience.go
@@ -1,7 +1,61 @@
 package main
 
-import "net/http"
+import (
+	"math"
+	"net/http"
+)
 
+const (
+	ROWS = 2000
+)
+
+// https://github.com/Jleagle/steamranks.com/blob/master/src/Application/Views/ExperienceView.php
 func experienceHandler(w http.ResponseWriter, r *http.Request) {
-	returnTemplate(w, "experience", nil)
+
+	template := experienceTemplate{}
+
+	template.Rows = append(template.Rows, experienceRow{
+		Level: 0,
+		Start: 0,
+	})
+
+	var xp float64
+	xp = 10
+
+	for i := 1; i <= ROWS+1; i++ {
+
+		diff := (math.Ceil((float64(i) + 1) / 10)) * 100
+
+		template.Rows = append(template.Rows, experienceRow{
+			Level: i,
+			Start: int(xp),
+		})
+
+		xp = xp + diff
+	}
+
+	for i := 1; i <= ROWS; i++ {
+
+		nextRow := template.Rows[i+1]
+		thisRow := template.Rows[i]
+
+		template.Rows[i].Diff = nextRow.Start - thisRow.Start
+		template.Rows[i].End = nextRow.Start - 1
+	}
+
+	template.Rows = template.Rows[0:ROWS]
+
+	returnTemplate(w, "experience", template)
+}
+
+type experienceTemplate struct {
+	Rows []experienceRow
+}
+
+type experienceRow struct {
+	Level int
+	Start int
+	End   int
+	Diff  int
+	Count int
 }
